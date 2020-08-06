24c44b0babf9e9632aec5a5a8be551d151b20fae
Maria <m.fernandalopez@MacBook-Pro.local>
2020-08-05 21:31:51 -0600

    crc2 stat correcta

--- stats.py
+++ stats.py
@@ -2,7 +2,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-results = [ True for i in range(11) ] + [ False for i in range(19) ]
 
 # stats para CRC32
 test_crc = open('results_crc.txt', 'r')
@@ -21,7 +20,7 @@ wrong = 0
 avg_time = 0
 
 for i in range(len(crc_times)):
-    if crc_vals[i] != str(results[i]):
+    if crc_vals[i] != 'False':
         wrong += 1
     avg_time += crc_times[i]
 
@@ -29,6 +28,9 @@ print('------------------- STATS CRC32 -------------------')
 print('Min time:', min(crc_times))
 print('Max time:', max(crc_times))
 print('Avg time:', str(avg_time/30))
+print('Incorrectly detected:', str(wrong))
+print('Correctly detected:', str(30-wrong))
+print('Accuracy:', str(((30-wrong)/30)*100) + '%')
 print('---------------------------------------------------')
 
 #stats para Hamming
@@ -57,6 +59,9 @@ print('------------------ STATS HAMMING ------------------')
 print('Min time:', min(ham_times))
 print('Max time:', max(ham_times))
 print('Avg time:', str(avg_time/22))
+print('Incorrectly corrected:', str(wrong))
+print('Correctly corrected:', str(22-wrong))
+print('Accuracy:', str(((22-wrong)/22)*100) + '%')
 print('---------------------------------------------------') 
     
 test_ham.close()
@@ -90,5 +95,54 @@ def times():
     fig.tight_layout()
     plt.show()
 
+def crc2Graph():
+    #Definimos una lista con paises como string
+    etiquetas = ['0-100', '100-200', '200 < ']
+    #Definimos una lista con ventas como entero
+    wrong = 0
+    for i in range(10):
+        if crc_vals[i] != 'False':
+            wrong += 1
+
+    wrong1 = 0
+    for i in range(11,20):
+        if crc_vals[i] != 'False':
+            wrong1 += 1
+    
+    wrong2 = 0
+    for i in range(21,30):
+        if crc_vals[i] != 'False':
+            wrong2 += 1
+
+   
+    res1 = [wrong, wrong1, wrong2]
+    res2 = [int(10-wrong), int(10-wrong1), int(10-wrong2)]
+
+    x = np.arange(len(etiquetas))
+    width = 0.35
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, res1, width, label='Correctos')
+    rects2 = ax.bar(x + width/2, res2, width, label='Incorrectos')
+    ax.set_ylabel('Cantidad de bits')
+    ax.set_title('Aciertos y Desaciertos CRC2')
+    ax.set_xticks(x)
+    ax.set_xticklabels(etiquetas)
+    ax.legend()
+
+    def autolabel(rects):
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+    autolabel(rects1)
+    autolabel(rects2)
+    fig.tight_layout()
+    plt.savefig('doble_barra.png')
+    #Mostramos la grafica con el metodo show()
+    plt.show()
 
 times()
+crc2Graph()
